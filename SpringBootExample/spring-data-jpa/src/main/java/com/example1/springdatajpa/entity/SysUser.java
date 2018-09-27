7e55249fcc53f86648bc81d83b9e39b08fac69e6
xiongzecheng <1249266311@qq.com>
2018-09-27 15:15:26 +0800

    ceshi

--- SpringBootExample/spring-data-jpa/src/main/java/com/example1/springdatajpa/entity/SysUser.java
+++ SpringBootExample/spring-data-jpa/src/main/java/com/example1/springdatajpa/entity/SysUser.java
@@ -29,7 +29,7 @@ public class SysUser implements Serializable {
     private  String password;
 
     private String nickname;
-    private Integer fans;
+    private Integer fans;//dfsfrgewrgksfksdhvkhfffgfd
 
 
 }
